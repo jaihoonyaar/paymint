1e068b11e67123ef92a55a4cfda4a004956e5a96
jaihoonyaar <jaiprakashrai733@gmail.com>
2026-03-10 00:50:18 +0530

    fix security config for auth endpoints for frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://paymint-backend-bb44.onrender.com",// your Spring Boot port
+  baseURL: "https://paymint-backend-bb44.onrender.com/api",// your Spring Boot port
 });
 
 // Attach JWT automatically
